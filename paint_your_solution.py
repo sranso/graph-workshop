48492853293d6610157bb6d94e0c81cebb9c60a5
Sarah Ransohoff <sranso@gmail.com>
2016-10-19 20:23:14 -0400

    Attempt paint problem, @sofiajeanne @pbiggar @rz.

--- paint_your_solution.py
+++ paint_your_solution.py
@@ -10,4 +10,21 @@ def paint_splash( pixels, click_point, target_color ):
     # YOUR CODE HERE. Modify pixels in place. Or create a new array, I don't care.
     # In place is better practice though.
 
+    x, y = click_point
+    color_at_click_point = pixels[x][y]
+    recurse_paint_splash( pixels, color_at_click_point, click_point, target_color )
     return pixels
+
+def recurse_paint_splash( pixels, og_color, current_point, target_color ):
+    print current_point
+    x, y = current_point
+    neighbors_indices = [ (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1) ]
+    try:
+        current_color = pixels[x][y]
+    except:
+        return
+    if current_color == og_color:
+        pixels[x][y] = target_color
+        for coords in neighbors_indices:
+            recurse_paint_splash( pixels, og_color, coords, target_color )
+
